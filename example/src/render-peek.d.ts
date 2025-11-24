470ff650823394c301a6c773c6174697c4843e35
Danil C. <55073929+chevgan@users.noreply.github.com>
2025-11-24 15:06:30 +0500

    Prepare for release: remove example app and update config

--- example/src/render-peek.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'render-peek' {
-    export interface RenderPeekOptions<P> {
-        ignoreKeys?: (keyof P)[];
-    }
-    export function useRenderPeek<P extends object>(
-        props: P,
-        options?: RenderPeekOptions<P>
-    ): { className: string };
-}
